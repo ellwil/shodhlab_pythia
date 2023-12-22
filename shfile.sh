fb293210c783048069e5ae426d267eb3960a1dd8
Ellwil Sharma <94732077+ellwil@users.noreply.github.com>
2023-12-22 18:01:55 +0530

    Add files via upload

---
+++ shfile.sh
@@ -0,0 +1,14 @@
+! /bin/bash
+SBATCH -N 1
+SBATCH --ntasks-per-node=32
+SBATCH --gres=gpu:A100-SXM4:1
+SBATCH --time=00:10:00
+SBATCH --error=job.%J.err
+SBATCH --output=job.%J.out
+echo "Starting at `date`"
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running $SLURM_NTASKS tasks."
+echo "Job id is $SLURM_JOBID"
+echo "Job submission directory is : $SLURM_SUBMIT_DIR"
+cd $SLURM_SUBMIT_DIR
