0c99fe4981fe187a137a3ead9c990e0f96e4dae0
Ellwil Sharma <94732077+ellwil@users.noreply.github.com>
2023-12-22 18:04:08 +0530

    Update shfile.sh

--- shfile.sh
+++ shfile.sh
@@ -1,8 +1,8 @@
 ! /bin/bash
 SBATCH -N 1
-SBATCH --ntasks-per-node=32
-SBATCH --gres=gpu:A100-SXM4:1
-SBATCH --time=00:10:00
+SBATCH --ntasks-per-node=1024
+SBATCH --gres=gpu:A100-SXM4:32
+SBATCH --time=20:10:00
 SBATCH --error=job.%J.err
 SBATCH --output=job.%J.out
 echo "Starting at `date`"
